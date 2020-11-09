cee178afde4751d4176e3da56cd2cd820e9ab3d9
juanroque <jroqueb1@gmail.com>
2020-11-09 08:23:01 -0400

    first commit

--- src/main/java/com/appsdeveloperblog/photoapp/api/albums/io/controllers/AlbumsController.java
+++ src/main/java/com/appsdeveloperblog/photoapp/api/albums/io/controllers/AlbumsController.java
@@ -5,29 +5,33 @@
  */
 package com.appsdeveloperblog.photoapp.api.albums.io.controllers;
 
-import com.appsdeveloperblog.photoapp.api.albums.data.AlbumEntity;
-import com.appsdeveloperblog.photoapp.api.albums.service.AlbumsService;
-import com.appsdeveloperblog.photoapp.api.albums.ui.model.AlbumResponseModel;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
+
 import org.modelmapper.ModelMapper;
-import java.lang.reflect.Type;
 import org.modelmapper.TypeToken;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
+import com.appsdeveloperblog.photoapp.api.albums.data.AlbumEntity;
+import com.appsdeveloperblog.photoapp.api.albums.service.AlbumsService;
+import com.appsdeveloperblog.photoapp.api.albums.ui.model.AlbumResponseModel;
+
 @RestController
 @RequestMapping("/users/{id}/albums")
 public class AlbumsController {
-    
+	Logger logger = LoggerFactory.getLogger(this.getClass());
+	
     @Autowired
-    AlbumsService albumsService;
-    Logger logger = LoggerFactory.getLogger(this.getClass());
+    AlbumsService albumsService;    
+    
     @GetMapping( 
             produces = { 
                 MediaType.APPLICATION_JSON_VALUE,
@@ -39,15 +43,17 @@ public class AlbumsController {
         
         List<AlbumEntity> albumsEntities = albumsService.getAlbums(id);
         
-        if(albumsEntities == null || albumsEntities.isEmpty())
-        {
+        if(albumsEntities == null || albumsEntities.isEmpty()){
             return returnValue;
         }
         
         Type listType = new TypeToken<List<AlbumResponseModel>>(){}.getType();
  
         returnValue = new ModelMapper().map(albumsEntities, listType);
-        logger.info("Returning " + returnValue.size() + " albums");
+        
+        logger.info("Returning " + returnValue.size() + " albums");        
         return returnValue;
     }
+    
+
 }
